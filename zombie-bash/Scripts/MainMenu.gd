d2be2e3acf1ac26acda0a1a15728b68531902cce
RobRob7 <rjames11@nmsu.edu>
2024-10-21 02:07:02 -0600

    New song for prototype

--- zombie-bash/Scripts/MainMenu.gd
+++ zombie-bash/Scripts/MainMenu.gd
@@ -9,7 +9,8 @@ func _on_play_button_pressed() -> void:
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear_to_db(Global.main_volume_value))
 	if get_tree().change_scene_to_file("res://Scenes/Game.tscn") != OK:
 		print ("Error changing scene to Game")
-		
+
+# settings button pressed
 func _on_settings_button_pressed() -> void:
 	if get_tree().change_scene_to_file("res://Scenes/Settings.tscn") != OK:
 		print ("Error changing scene to Settings")
@@ -17,3 +18,8 @@ func _on_settings_button_pressed() -> void:
 # quit button pressed (close window)
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+# credits button is pressed
+func _on_credits_button_pressed() -> void:
+	if get_tree().change_scene_to_file("res://Scenes/Credits.tscn") != OK:
+		print ("Error changing scene to Credits")
